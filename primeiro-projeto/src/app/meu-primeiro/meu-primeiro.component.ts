e3c4ae36deff8d612577a1b6de1dd066cbff93f7
Rafael Santos <rsanthos@gmail.com>
2017-09-10 18:29:57 -0300

    aula 03

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro componente com angular 4</p>
+    `
+})
+export class MeuPrimeiroComponent { }
